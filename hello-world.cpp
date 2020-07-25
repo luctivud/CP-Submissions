ab8a8edacc4ee5e581361b3f9806921640f7089e
luctivud <luctivud@gmail.com>
2020-07-25 12:02:25 +0530

    first commit

---
+++ hello-world.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int x; cin >> x;
+
+    cout << "Hello World\n";
+    for (int i =0 ; i< x ; i++) {
+        if (i & 1) cout << "not ";
+        cout << "ok\n";
+    }
+    return 0;
+}

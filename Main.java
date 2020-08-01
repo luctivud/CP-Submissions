ece616bdd539ad94963a192505e4f865fac66048
luctivud <luctivud@gmail.com>
2020-08-01 09:03:59 +0530

    Prc

--- Main.java
+++ Main.java
@@ -3,26 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         final Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int n = str.length();
-        String target = new String("keyence");
-        // System.out.println(n);
-        for(int i=0; i<n; i++) {
-            for (int j=0; j <n; j++) {
-                // remove i to j
-                String temp = new String("");
-                for (int k=0; k <n; k++) {
-                    if (k<i || k >j) temp += str;
-                }
-
-                if (temp.equals(target)) {
-
-                    System.out.println("YES");
-                    return;
-                }
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        while (k > 0) {
+            if ((n % 10) == 0) {
+                n /= 10;
+            } else {
+                n -= 1;
             }
+            k -= 1;
+            // System.out.println(n);
         }
-        System.out.println("NO");
+        System.out.println(n);
         sc.close();
     }
 }

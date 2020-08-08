3879d19eea84d4fd69ae9bd36ba2816023d71563
luctivud <luctivud@gmail.com>
2020-08-08 11:06:55 +0530

    PRc

--- cc-SKMP.py
+++ cc-SKMP.py
@@ -1,16 +1,35 @@
-for __ in range(int(input())):
-	s = input()
-	p = input()
-	from collections import Counter
-	c = Counter(s)
-	for i in p:
-		c[i] -= 1
-	first_letter = p[0]
-	for k, v in sorted(c.items()):
-		if k <= first_letter:
-			print(k * v, end="")
-	print(p, end ="")
-	for k, v in sorted(c.items()):
-		if k > first_letter:
-			print(k * v, end="")
-	print()
+def get_ints() : return map(int, input().split())
+def get_list() : return list(get_ints())
+import sys 
+sys.setrecursionlimit(int(1e5))
+
+
+def solve(i, table, number_of_tables, number_of_extras):
+	# print(i, table, number_of_tables, number_of_extras)
+	if (i >= n):
+		return number_of_extras + number_of_tables * k
+	currCost = number_of_extras + number_of_tables * k
+	if li[i] in table:
+		if currCost not in dp[i]:
+			if table[li[i]] == 1:
+				table[li[i]] += 1
+				dp[i][currCost] = min(solve(i+1, {li[i] : 1}, number_of_tables+1, number_of_extras), \
+					solve(i+1, table, number_of_tables, number_of_extras+2))
+			else:
+				table[li[i]] += 1
+				dp[i][currCost] = min(solve(i+1, {li[i] : 1}, number_of_tables+1, number_of_extras), \
+					solve(i+1, table, number_of_tables, number_of_extras+1))
+	else:
+		table[li[i]] = 1
+		if currCost not in dp[i]:
+			dp[i][currCost] = solve(i+1, table, number_of_tables, number_of_extras)
+
+	return dp[i][currCost]
+
+from collections import defaultdict
+for _ in range(int(input())):
+	n, k = get_ints()
+	li = get_list()
+	dp = [dict() for x in range(1005)]
+	print(solve(0, {}, 1, 0))		
+	# print(dp)

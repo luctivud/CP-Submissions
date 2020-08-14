f25d5abe5b0edfbb88178b2729bf3d7dc78ea441
luctivud <luctivud@gmail.com>
2020-08-14 18:05:39 +0530

    prc

--- cc-ACCBIP.py
+++ cc-ACCBIP.py
@@ -18,48 +18,14 @@ def get_list()      : return list(get_ints())
 INF = float('inf')
 
 
-# precompute nc3 for n's using the recurrence relation : C3(n) = (n / (n-3)) * C3(n-1)
-NC3_list = [0, 0, 0, 1]
-def precomputeNC3(n):
-	for i in range(4, n+1):
-		NC3_list.append(int((i/(i-3)) * NC3_list[i-1]))
-	# print(NC3_list[-1])
 
-# helper to return NC3_list item
-def getNC3(n: int):
-	return NC3_list[n]
+
+# Util to calculate Triangles
 
 
 
-# Util to calculate Triangles:
-def calculateTriangles(color_count):
-	ans = 0
-	for i in color_count:
-		ans += getNC3(i)
-	return ans
+# Using dp to solve knapsack N*k
 
-
-
-# recursively solve using dynamic programming => memoization of tuples
-def solve(eraser_length, color_count):
-	# print(eraser_length, color_count)
-	# tupled_color_count = tuple(color_count)
-
-	if eraser_length not in dp:
-		ok = False	
-		mini = INF
-		# find minimum triangles for every turn till eraser is availb
-		for i in range(c):
-			if (eraser_length - v[i] >= 0 and color_count[i] > 0):
-				ok = True
-				color_count_copy = color_count.copy()
-				color_count_copy[i] -= 1
-				xen = solve(eraser_length - v[i], color_count_copy)
-				mini = min(mini, xen)
-		if not ok:
-			mini = calculateTriangles(color_count)
-		dp[eraser_length] = mini
-	return dp[eraser_length]
 
 
 
@@ -67,19 +33,22 @@ def solve(eraser_length, color_count):
 
 
 # MAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-precomputeNC3(3005)	# precomputeNC3's for O(1) access
 
 
 for _test_ in range(int(input())): # Test Cases
 	n, c, k = get_ints()
-	color_count = [0] * (c)
+	# color count to store count of slopes of a particukar type
+	color_count = [defaultdict(int) for x in range(c)] 
 	for i in range(n):
 		ai, bi, ci = get_ints()
-		color_count[ci-1] += 1
-	v = get_list()
-	dp = dict()
+		color_count[ci-1][ai] += 1
+	# cost of erasing a line
+	cost_erase = get_list()
+	
+	dp = [[-1] * (k+1) for x in range(n)]
+	# print(color_count)
 
-	print(solve(k, color_count))
+	initialTriangles = calculateTriangles()
 	# print(dp)
 
 
@@ -103,3 +72,52 @@ for _test_ in range(int(input())): # Test Cases
 
 
 
+
+# # precompute nc3 for n's using the recurrence relation : C3(n) = (n / (n-3)) * C3(n-1)
+# NC3_list = [0, 0, 0, 1]
+# def precomputeNC3(n):
+# 	for i in range(4, n+1):
+# 		NC3_list.append(int((i/(i-3)) * NC3_list[i-1]))
+# 	# print(NC3_list[-1])
+
+# # helper to return NC3_list item
+# def getNC3(n: int):
+# 	return NC3_list[n]
+
+
+# precomputeNC3(3005)	# precomputeNC3's for O(1) access
+
+
+
+# # Util to calculate Triangles:
+# def calculateTriangles(color_count):
+# 	ans = 0
+# 	for i in color_count:
+# 		ans += getNC3(i)
+# 	return ans
+
+
+
+
+
+
+# # recursively solve using dynamic programming => memoization of tuples
+# def NotSolve(eraser_length, color_count):
+# 	# print(eraser_length, color_count)
+# 	# tupled_color_count = tuple(color_count)
+
+# 	if eraser_length not in dp:
+# 		ok = False	
+# 		mini = INF
+# 		# find minimum triangles for every turn till eraser is availb
+# 		for i in range(c):
+# 			if (eraser_length - v[i] >= 0 and color_count[i] > 0):
+# 				ok = True
+# 				color_count_copy = color_count.copy()
+# 				color_count_copy[i] -= 1
+# 				xen = solve(eraser_length - v[i], color_count_copy)
+# 				mini = min(mini, xen)
+# 		if not ok:
+# 			mini = calculateTriangles(color_count)
+# 		dp[eraser_length] = mini
+# 	return dp[eraser_length]

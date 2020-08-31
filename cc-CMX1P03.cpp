2532beafc9b25e7d06409bc129db8d159b876507
Udit Gupta <luctivud@gmail.com>
2020-08-31 22:17:44 +0530

    codechef comp encoding

---
+++ cc-CMX1P03.cpp
@@ -0,0 +1,211 @@
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%O:,*..***%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%O. .:&Oo.,&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%O. .l%%%o..l*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%O*****************************************%%%%%&*olo*&%%<  .:***;  ,****&*%*&*************************************%%
+%&<****************.  ..*****.   .****.  .**%%%%*..,:. .**o**.  .**.  ..**;&%&;*****.  .*.**.  .******.  ..**.   .**%%
+%%%*%%%%%%%%%%%%%%%O* ;&%%%%%&o. <&%%%*. l*%%%%&; ,0%*. :%%**l .*%%O* ;&%%%%%%%%%%%*< .&%%%%*. <*%%%%%O* ;&%%0; *O%%%%
+%%%%%%%%%%%%%%%%%%%0* :%%%%%%%*: .O%%%*. o%%%%%%*. *;. .*%%%%o .*%%0* :%%%%%%%%%%%%%l .O%%%%&. l%%%%%%0* ;*%%*: ,&%%%%
+%%%%%%%%%&<<<<<<<<<;. :%%%%%%&*. :*%%%*. o%%%%%&o. .*,. .,l**; .*%%0* :%%%%%%%%%%%%%l .O%%%%&. l%%%%%%0* ;*%%*: ,0%%%%
+%%%*&%%%%*<<,. .;<<;. :*%&,**..*l&%%%%*. o%%%*:. .lO%%&*l.     .*%%0* :%%%%%%%0:,o&O* ,&%%%%&. l%%%&o<,. .;::;. ,&%%%%
+%%*<.l*%%%%%%*..*%%0* :%%0;  ;&*%%%%%%l  o%%&:.;**%%%%**:. ,<* .*%%0* :%%%%%%%&<. ...;&%%%%%&. l%%< .:;. .<l<:. ,&%%%%
+%%%*. l*%%%%%&, l%%0* :%%%%**.,l***o<*   o%%%%&%%%%%0o* .:&%%o .*%%0* :%%%%%%%%%O;  ,O%%%%%%&. l%%* l**. l%%%*: ,&%%%%
+%%%%&*.,*&%*0<..&%%0* :%%%%%%&l;,**,:l:  o%%%%%%%%%O* *l0%%%%o .*%%0* :%%%%%%%%%%%*. .o*%%%%&. l%%*.....;0%%%*: ,&%%%%
+%%%%%&o*...*..:&%%%0* :%%%%%%%%%%%%%%%*. o%%%%%%%%%%O**%%%%%%o .*%%0* :%%%%%%%%%%%%&l. ,&%%%&. l%%%&&**0%%%%%*: ,&%%%%
+%%%%%%%%0&*&0*%%%%%&l;*%%%%%%%%%%%%%%%0<;&%%%%%%%%%%%%%%%%%%%&;:O%%*l,*%%%%%%%%%%%%%%O; .*%%0<;&%%%%%%%%%%%%%%*,o*%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&*&%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+//             Author: Udit "luctivud" Gupta @ (https://www.linkedin.com/in/udit-gupta-1b7863135/)                  //
+
+
+
+#include <bits/stdc++.h>
+#pragma GCC optimize "trapv"
+
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// template <typename T> using PBSET = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+/*
+    .insert(el) - set hai!
+    .find_by_order(3) - returns an iterator to the k-th largest element (counting from zero)
+    .order_of_key(6) - the number of items in a set that are strictly smaller than our item
+*/
+
+typedef long long int lld;
+typedef unsigned long long int llu;
+
+#define         TESTCASES()    cin >> (T3X0); T353 = T3X0; while(T3X0--)
+#define          input(V3C)    for(auto &V3C_I7 : (V3C)) cin >> (V3C_I7)
+#define   mems(A77AY, V4LU)    memset((A77AY), (V4LU), sizeof((A77AY)))
+#define    CH3K(I7, E4, S7)    (((S7)>0) ? (I7)<(E4) : (I7)>(E4))
+#define   for4(I7,S4,E4,S7)    for(auto I7=(S4); CH3K(I7,E4,S7); (I7)+=(S7))
+#define        forn(I7, E4)    for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define        EACH(I7, A7)    for (auto& I7: A7)
+#define              len(v)    ((int)((v).size()))
+#define              all(x)    (x).begin(), (x).end()
+#define             rall(x)    (x).rbegin(), (x).rend()
+#define                  pb    push_back
+#define         debspace(x)    cout << #x << " = "; println(x);
+#define          debline(x)    cout << #x << " = "; print(x); cout << " ";
+#define                  f1    first
+#define                  s2    second
+
+
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); huehue(_it, args); cout << "\n";}
+
+void huehue(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void huehue(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << ", ";
+	huehue(++it, args...);
+}
+
+
+template <class T> T inf() {
+	return numeric_limits<T>::max();
+}
+
+
+void read() { return; }
+void print() { return; }
+void println() { cout << "\n"; return; }
+template<class T> T read(T& x)   { cin >> x; return x; }
+template<class T> void print(T a)   { cout << a; }
+template<class T> void println(T a) { cout << a << "\n"; }
+
+template<class T> void read(vector<T> &arr)   { EACH(i, arr) cin >> (i); }
+template<class T> void print(vector<T> arr)   { EACH(i, arr) {cout << i << " ";} }
+template<class T> void println(vector<T> arr) { EACH(i, arr) {cout << i << " ";} cout << "\n"; }
+
+template<class T> void read(vector<vector<T>> &arr)   { EACH(i, arr) read(i); }
+template<class T> void print(vector<vector<T>> arr)   { EACH(i, arr) println(i); }
+template<class T> void println(vector<vector<T>> arr) { EACH(i, arr) println(i); }
+
+template<typename T, typename... Args> void read(vector<T> &arr, Args &... args)   { read(arr); read(args...);}
+template<typename T, typename... Args> void read(vector<vector<T>> &arr, Args &... args)   { read(arr); read(args...);}
+template<typename T, typename... Args> void read(T &a, Args &... args) { cin >> (a); read(args...); }
+template<typename T, typename... Args> void print(vector<T> &arr, Args &... args)   { print(arr); print(args...);}
+template<typename T, typename... Args> void print(T a, Args... args) { cout << a << " "; print(args...); };
+template<typename T, typename... Args> void println(vector<T> &arr, Args &... args)   { print(arr); println(args...);}
+template<typename T, typename... Args> void println(T a, Args... args) { cout << a << " "; println(args...); };
+
+
+const lld d4i[4] = { -1, 0, 1, 0}, d4j[4] = {0, 1, 0, -1};
+const lld d8i[8] = { -1, -1, 0, 1, 1, 1, 0, -1}, d8j[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const lld mod = 998244353;
+
+
+lld EE(lld a, lld b, lld *x, lld *y) {
+	if (a == 0) {
+		*x = 0, *y = 1;
+		return b;
+	}
+
+	lld x1, y1;
+	lld gcd = EE(b % a, a, &x1, &y1);
+
+	*x = y1 - (b / a) * x1;
+	*y = x1;
+
+	return gcd;
+}
+
+
+lld inverse(lld a, lld m) {
+	lld x, y;
+	EE(a, m, &x, &y);
+	if (x < 0) x += m;
+	return x;
+}
+
+lld choose(lld n, lld k) {
+	lld i;
+
+	lld num = 1, den = 1;
+
+	for (i = 2; i <= k; i++) {
+		den *= i;
+		den %= mod;
+	}
+
+	for (i = n - k + 1; i <= n; i++) {
+		num *= i;
+		num %= mod;
+	}
+
+	i = num * inverse(den, mod);
+	i %= mod;
+	return i;
+}
+
+
+
+
+void solveEachTest(lld _TestCase = 1) {
+	// cout << "Case#" << _TestCase << ": ";
+	string hairu; read(hairu);
+	lld mainCount[26] = {0};
+	EACH(it, hairu) {
+		mainCount[it - 'a']++;
+	}
+
+	// forn(i, 26) {
+	// 	print(mainCount[i], "");
+	// }
+	// debspace(mainCount);
+	lld n; read(n);
+
+	string ansName = "No Research This Month";
+	lld maxCC = 0;
+	forn(tt, n) {
+		string temp; read(temp);
+		lld thisCount[26] = {0};
+		EACH(it, temp) {
+			thisCount[it - 'a']++;
+		}
+
+		lld thisMaxCC = 1ll;
+		forn(i, 26) {
+			if (thisCount[i] > mainCount[i]) {
+				thisMaxCC = 0ll;
+				break;
+			}
+
+			if (mainCount[i] == 0 or thisCount[i] == 0) continue;
+			// error(tt, mainCount[i], thisCount[i], i);
+			thisMaxCC *= choose(mainCount[i], thisCount[i]);
+			thisMaxCC %= mod;
+		}
+		thisMaxCC %= mod;
+		println(thisMaxCC);
+		if (thisMaxCC > maxCC) {
+			maxCC = thisMaxCC;
+			ansName = temp;
+		}
+	}
+	println(ansName);
+
+
+	// cout << "\n";
+	return;
+}
+
+
+signed main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.precision(10); cout << fixed;
+
+	lld T3X0 = 0, T353 = 1;
+
+	TESTCASES()
+	solveEachTest(T353 - T3X0);
+	return 0;
+}
+// Random Thought :  ja ja

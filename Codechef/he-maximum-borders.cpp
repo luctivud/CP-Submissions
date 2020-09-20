fa3e39f834083c18bdf162bcc2484ddf6be5f1d0
Udit Gupta <luctivud@gmail.com>
2020-09-20 21:27:47 +0530

    he contest

---
+++ Codechef/he-maximum-borders.cpp
@@ -0,0 +1,251 @@
+// ************************* J A I  S H R E E  R A M  *************************
+
+
+/*
+
+      :: All is One ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+      Author: Udit "luctivud" Gupta
+      linkedin : (https://www.linkedin.com/in/udit-gupta-1b7863135/)
+
+      My original ids go by the username @luctivud
+      However, if summoned, my alts can surround your surroundings in a flash.
+
+      Do you know Hairu Ihei ?
+
+      :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: One is All ::
+
+
+*/
+
+
+#include <bits/stdc++.h>
+
+#pragma GCC optimize "trapv"
+#pragma GCC optimize ("Ofast")
+// #pragma GCC target ("fpmath=sse,sse2")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("-ffloat-store")
+
+
+using namespace std;
+
+
+
+typedef long long int lld;
+typedef unsigned long long int llu;
+
+#define         TESTCASES()    cin >> (T3X0); T353 = T3X0; while(T3X0--)
+#define          input(V3C)    for(auto &V3C_I7 : (V3C)) cin >> (V3C_I7)
+#define   mems(A77AY, V4LU)    memset((A77AY), (V4LU), sizeof((A77AY)))
+#define    CH3K(I7, E4, S7)    (((S7)>0) ? (I7)<(E4) : (I7)>(E4))
+#define   for4(I7,S4,E4,S7)    for(auto I7=(S4); CH3K(I7,E4,S7); (I7)+=(S7))
+#define        forn(I7, E4)    for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define       forn1(I7, E4)    for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
+#define        EACH(I7, A7)    for (auto& I7: A7)
+#define              len(v)    ((int)((v).size()))
+#define              all(x)    (x).begin(), (x).end()
+#define             rall(x)    (x).rbegin(), (x).rend()
+#define                  pb    push_back
+#define         debhairu(x)    cout << #x << " = "; println(x);
+#define         debspace(x)    cout << #x << " = "; print(x); cout << " ";
+#define                  f1    first
+#define                  s2    second
+
+
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); huehue(_it, args); cout << "\n";}
+
+void huehue(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void huehue(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << ", ";
+	huehue(++it, args...);
+}
+
+
+template <class T> T inf() {
+	return numeric_limits<T>::max();
+}
+
+
+void read() { return; }
+void print() { return; }
+void println() { cout << "\n"; return; }
+template<class T> T read(T& x)   { cin >> x; return x; }
+template<class T> void print(T a)   { cout << a; }
+template<class T> void println(T a) { cout << a << "\n"; }
+
+template<class T> void read(vector<T> &arr)   { EACH(i, arr) cin >> (i); }
+template<class T> void print(vector<T> arr)   { EACH(i, arr) {cout << i << " ";} }
+template<class T> void println(vector<T> arr) { EACH(i, arr) {cout << i << " ";} cout << "\n"; }
+
+template<class T> void read(vector<vector<T>> &arr)   { EACH(i, arr) read(i); }
+template<class T> void print(vector<vector<T>> arr)   { EACH(i, arr) println(i); }
+template<class T> void println(vector<vector<T>> arr) { EACH(i, arr) println(i); }
+
+template<typename T, typename... Args> void read(vector<T> &arr, Args &... args)   { read(arr); read(args...);}
+template<typename T, typename... Args> void read(vector<vector<T>> &arr, Args &... args)   { read(arr); read(args...);}
+template<typename T, typename... Args> void read(T &a, Args &... args) { cin >> (a); read(args...); }
+template<typename T, typename... Args> void print(vector<T> &arr, Args &... args)   { print(arr); print(args...);}
+template<typename T, typename... Args> void print(T a, Args... args) { cout << a << " "; print(args...); };
+template<typename T, typename... Args> void println(vector<T> &arr, Args &... args)   { print(arr); println(args...);}
+template<typename T, typename... Args> void println(T a, Args... args) { cout << a << " "; println(args...); };
+
+
+const lld d4i[4] = { -1, 0 }, d4j[4] = {0, 1};
+const lld d8i[8] = { -1, -1, 0, 1, 1, 1, 0, -1}, d8j[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+const auto start_time = std::chrono::high_resolution_clock::now();
+signed luctivud() {
+
+#ifdef LUCTIVUD
+	auto end_time = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = end_time - start_time;
+	cerr << "Finished in : " << diff.count() << "\n";
+#endif
+
+	return 0;
+} 
+
+
+const lld MOD = lld(1e9) + 7ll;
+const lld mod = MOD;
+
+
+const long double EPS = 1e-6;
+
+const lld INFll = inf<lld>();
+const int INFi = inf<int>();
+
+
+void add_undirected_edge(lld a, lld b, vector<lld> adj[]) {
+	adj[a].pb(b);
+	adj[b].pb(a);
+	return;
+}
+
+
+
+/*
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+*/
+
+lld mx = 0ll;
+lld n, m;
+
+lld cnt = 0ll;
+lld next_i, next_j;
+
+bool isvalid(lld i, lld j) {
+	return i >= 0 and j >= 0 and i < n and j < m;
+}
+
+
+
+void traverse
+(
+    lld i, lld j, vector<vector<char>> grid, set<pair <lld, lld>> &visited,
+    lld up = 0ll, lld down = 0ll, lld left = 0ll, lld right = 0ll
+)
+{
+	if (!isvalid(i, j)) return;
+	cnt++;
+	visited.insert({i, j});
+	if (i - 1 < 0 or grid[i - 1][j] != '#') up += 1;
+	if (i + 1 >= n or grid[i + 1][j] != '#') down += 1;
+	if (j - 1 < 0 or grid[i][j - 1] != '#') left += 1;
+	if (j + 1 >= m or grid[i][j + 1] != '#') right += 1;
+	// error(i, j, up, down, left, right);
+	mx = max({mx, left, right, up, down});
+	forn(k, 4) {
+		lld newI = i + d4i[k];
+		lld newJ = j + d4j[k];
+		if (isvalid(newI, newJ) and (visited.find({newI, newJ}) == visited.end()) and grid[newI][newJ] == '#') {
+			// traverse(newI, newJ, grid, visited, ((d4i[k] == 0) ? 0ll : up), ((d4i[k] == 0) ? 0ll : down), ((d4j[k] == 0) ? 0ll : left), ((d4j[k] == 0) ? 0ll : right));
+			if (d4i[k] == 0) {
+				traverse(newI, newJ, grid, visited, up, down, 0ll, 0ll);
+			}
+
+			if (d4j[k] == 0) {
+				traverse(newI, newJ, grid, visited, 0ll, 0ll, left, right);
+			}
+
+			next_j = max(next_j, newJ+1);
+			next_i = max(next_i, newI+1);
+		}
+	}
+}
+
+
+
+
+
+
+void solveEachTest(lld _TestCase) {
+	// cout << "Case#" << _TestCase << ": ";
+	read(n, m);
+	mx = 0ll;
+	cnt = 0ll;
+	vector<vector<char>> matr(n, vector<char>(m)) ;	read(matr);
+
+	// println(matr);
+
+	// vector <vector<bool>> visited(n, vector<bool> (m, false));
+	set<pair<lld, lld>> visited;
+
+	next_i = 0, next_j = 0;
+	lld i = 0ll, j = 0ll;
+	// while (i < n) {
+	// 	while (j < m) {
+	// 		error(i, j);
+	// 		next_i = max(next_i, i);
+	// 		next_j = max(next_j, j);
+	// 		// error(i, j);
+	// 		if (visited.find({i, j}) == visited.end() and i < n and j < m and matr[i][j] == '#') {
+	// 			traverse(i, j, matr, visited);
+	// 		}
+	// 		if (i == next_i) i++; else i = next_i+1;
+	// 		if (j == next_j) j++; else j = next_j+1;
+	// 	}
+
+	// }
+
+
+	while (i < n and j < m) {
+		// error(i, j);
+		if (visited.find({i, j}) == visited.end() and i < n and j < m and matr[i][j] == '#') {
+			traverse(i, j, matr, visited);
+		}
+		i = next_i, j = next_j;
+		j++;
+		next_i = i, next_j = j;
+		// if (j >= m) {
+		// 	j = 0; i++;
+		// }
+	}
+
+	// println(mx, cnt, n, m);
+	println(mx);
+
+	// cout << "\n";
+	return;
+}
+
+
+signed main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	// cout.precision(10); cout << fixed;
+
+	lld T3X0 = 0, T353 = 1;
+
+	TESTCASES()
+	solveEachTest(T353 - T3X0);
+
+	return luctivud();
+}
+
+// Random Thought :  null
+/*
+        My lungs will fill and then deflate.
+*/

a47f3513e7258ada018c48c17802614422061bd9
Udit Gupta <luctivud@gmail.com>
2020-09-25 11:06:23 +0530

    cf round 1420

---
+++ Codeforces/1420a.cpp
@@ -0,0 +1,283 @@
+// ************************.- J A I  S H R E E  R A M -.*************************
+
+
+/*
+      ::: All is One ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+          Author   :  Udit "luctivud" Gupta
+
+          linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+          github   :  https://github.com/luctivud
+
+      :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: One is All :::
+*/
+
+
+#include <bits/stdc++.h>
+
+#pragma GCC optimize "trapv"
+#pragma GCC optimize ("Ofast")
+// #pragma GCC target ("fpmath=sse,sse2")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize ("-ffloat-store")
+
+
+using namespace std;
+
+
+
+typedef long long int lld;
+typedef unsigned long long int llu;
+
+#define         TESTCASES()    cin >> (T3X0); T353 = T3X0; while(T3X0--)
+#define          input(V3C)    for(auto &V3C_I7 : (V3C)) cin >> (V3C_I7)
+#define   mems(A77AY, V4LU)    memset((A77AY), (V4LU), sizeof((A77AY)))
+#define    CH3K(I7, E4, S7)    (((S7)>0) ? (I7)<(E4) : (I7)>(E4))
+#define   for4(I7,S4,E4,S7)    for(auto I7=(S4); CH3K(I7,E4,S7); (I7)+=(S7))
+#define        forn(I7, E4)    for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define       forn1(I7, E4)    for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
+#define        EACH(I7, A7)    for (auto& I7: A7)
+#define              len(v)    ((int)((v).size()))
+#define              all(x)    (x).begin(), (x).end()
+#define             rall(x)    (x).rbegin(), (x).rend()
+#define                  pb    push_back
+#define         debhairu(x)    cout << #x << " = "; println(x);
+#define         debspace(x)    cout << #x << " = "; print(x); cout << " ";
+#define                  f1    first
+#define                  s2    second
+#define           PLEASE_AC    return
+
+
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); huehue(_it, args); cout << "\n";}
+
+void huehue(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void huehue(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << ", ";
+	huehue(++it, args...);
+}
+
+
+template <class T> T inf() {
+	return numeric_limits<T>::max();
+}
+
+
+void read() { return; }
+void print() { return; }
+void println() { cout << "\n"; return; }
+template<class T> T read(T& x)   { cin >> x; return x; }
+template<class T> void print(T a)   { cout << a; }
+template<class T> void println(T a) { cout << a << "\n"; }
+
+template<class T> void read(vector<T> &arr)   { EACH(i, arr) cin >> (i); }
+template<class T> void print(vector<T> arr)   { EACH(i, arr) {cout << i << " ";} }
+template<class T> void println(vector<T> arr) { EACH(i, arr) {cout << i << " ";} cout << "\n"; }
+
+template<class T> void read(vector<vector<T>> &arr)   { EACH(i, arr) read(i); }
+template<class T> void print(vector<vector<T>> arr)   { EACH(i, arr) println(i); }
+template<class T> void println(vector<vector<T>> arr) { EACH(i, arr) println(i); }
+
+template<typename T, typename... Args> void read(vector<T> &arr, Args &... args)   { read(arr); read(args...);}
+template<typename T, typename... Args> void read(vector<vector<T>> &arr, Args &... args)   { read(arr); read(args...);}
+template<typename T, typename... Args> void read(T &a, Args &... args) { cin >> (a); read(args...); }
+template<typename T, typename... Args> void print(vector<T> &arr, Args &... args)   { print(arr); print(args...);}
+template<typename T, typename... Args> void print(T a, Args... args) { cout << a << " "; print(args...); };
+template<typename T, typename... Args> void println(vector<T> &arr, Args &... args)   { print(arr); println(args...);}
+template<typename T, typename... Args> void println(T a, Args... args) { cout << a << " "; println(args...); };
+
+
+const lld d4i[4] = { -1, 0, 1, 0}, d4j[4] = {0, 1, 0, -1};
+const lld d8i[8] = { -1, -1, 0, 1, 1, 1, 0, -1}, d8j[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+
+const auto start_time = std::chrono::high_resolution_clock::now();
+
+signed light() {
+
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	cout.precision(10); cout << fixed;
+
+#ifdef LUCTIVUD
+	freopen("/home/luctivud/Streams/IO/input.txt", "r", stdin);
+	freopen("/home/luctivud/Streams/IO/output.txt", "w", stdout);
+#endif
+
+	return 0;
+}
+
+
+signed luctivud() {
+
+#ifdef LUCTIVUD
+	auto end_time = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = end_time - start_time;
+	cerr << "Finished in : " << diff.count() << "\n";
+#endif
+
+	return 0;
+}
+
+
+
+const lld MOD = lld(1e9) + 7ll;
+const lld mod = MOD;
+
+
+const long double EPS = 1e-6;
+
+const lld INFll = inf<lld>();
+const int INFi = inf<int>();
+
+
+void add_undirected_edge(lld a, lld b, vector<lld> adj[]) {
+	adj[a].pb(b);
+	adj[b].pb(a);
+	return;
+}
+
+
+
+/*
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+*/
+
+// taken from https://www.geeksforgeeks.org/number-swaps-sort-adjacent-swapping-allowed/
+
+#define int lld
+
+int merge(int arr[], int temp[], int left, int mid, int right)
+{
+	int inv_count = 0;
+
+	int i = left; /* i is index for left subarray*/
+	int j = mid;  /* i is index for right subarray*/
+	int k = left; /* i is index for resultant merged subarray*/
+	while ((i <= mid - 1) && (j <= right))
+	{
+		if (arr[i] <= arr[j])
+			temp[k++] = arr[i++];
+		else
+		{
+			temp[k++] = arr[j++];
+
+			/* this is tricky -- see above explanation/
+			  diagram for merge()*/
+			inv_count = inv_count + (mid - i);
+		}
+	}
+
+	/* Copy the remaining elements of left subarray
+	 (if there are any) to temp*/
+	while (i <= mid - 1)
+		temp[k++] = arr[i++];
+
+	/* Copy the remaining elements of right subarray
+	 (if there are any) to temp*/
+	while (j <= right)
+		temp[k++] = arr[j++];
+
+	/*Copy back the merged elements to original array*/
+	for (i = left; i <= right; i++)
+		arr[i] = temp[i];
+
+	return inv_count;
+}
+
+/* An auxiliary recursive function that sorts the input
+   array and returns the number of inversions in the
+   array. */
+int _mergeSort(int arr[], int temp[], int left, int right)
+{
+	int mid, inv_count = 0;
+	if (right > left)
+	{
+		/* Divide the array into two parts and call
+		  _mergeSortAndCountInv() for each of the parts */
+		mid = (right + left) / 2;
+
+		/* Inversion count will be sum of inversions in
+		   left-part, right-part and number of inversions
+		   in merging */
+		inv_count  = _mergeSort(arr, temp, left, mid);
+		inv_count += _mergeSort(arr, temp, mid + 1, right);
+
+		/*Merge the two parts*/
+		inv_count += merge(arr, temp, left, mid + 1, right);
+	}
+
+	return inv_count;
+}
+
+/* This function sorts the input array and returns the
+   number of inversions in the array */
+int countSwaps(int arr[], int n)
+{
+	int temp[n];
+	return _mergeSort(arr, temp, 0, n - 1);
+}
+
+
+
+
+
+void solveEachTest(lld _TestCase) {
+	// cout << "Case#" << _TestCase << ": ";
+	lld n; read(n);
+	lld arr[n];
+
+	forn(i, n) {
+		read(arr[i]);
+	}
+
+	// map<lld, lld> pos;
+
+	// forn(i, n) {
+	// 	pos[arr[i]] = i;
+	// }
+
+	// vector<lld> brr{arr};
+	// sort(all(brr));
+
+	// lld total = 0ll;
+	// forn(i, n) {
+	// 	if (arr[i] != brr[i] and pos[arr[i]] > pos[brr[i]]) {
+	// 		total += pos[arr[i]] - pos[brr[i]];
+	// 	}
+	// }
+
+
+
+	lld total = countSwaps(arr, n);
+	// error(total);
+
+	if ((total + 1) <= (n * (n - 1)) / 2) {
+		println("YES");
+	} else {
+		println("NO");
+	}
+
+
+
+	// cout << "\n";
+	return;
+}
+
+
+signed main() {
+
+	light(); lld T3X0 = 0, T353 = 1;
+
+	TESTCASES()
+	solveEachTest(T353 - T3X0);
+
+	PLEASE_AC luctivud();
+}
+
+// Random Thought :  null
+/*
+        My lungs will fill and then deflate
+        They fill with fire, exhale desire
+*/

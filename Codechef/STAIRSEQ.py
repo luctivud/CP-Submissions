d7f6bb43775c5de9d2abaa328b6aa4c8090a1a5e
Udit Gupta <luctivud@gmail.com>
2021-04-14 10:56:39 +0530

    codechef contest

---
+++ Codechef/STAIRSEQ.py
@@ -0,0 +1,40 @@
+#				 Author: Udit "luctivud" Gupta @ https://www.linkedin.com/in/udit-gupta-1b7863135/					 #
+
+
+import math;   		from collections import *
+import sys;   		from functools import reduce
+import time;   		from itertools import groupby
+
+# sys.setrecursionlimit(10**6)
+
+# def input()         : return sys.stdin.readline()
+def get_ints()      : return map(int, input().strip().split())
+def get_list()      : return list(get_ints())
+def get_string()    : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+DIRECTIONS = [(+0, +1), (+0, -1), (+1, +0), (+1, -1)] 
+NEIGHBOURS = [(-1, -1), (-1, +0), (-1, +1), (+0, -1),\
+              (+1, +1), (+1, +0), (+1, -1), (+0, +1)]
+
+
+
+# MAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# for _test_ in range(int(input())): 
+n = int(input())
+li = sorted(get_list(), reverse = True)
+q = deque()
+q.append(li[0])
+for i in range(1, n):
+	if q[-1] != li[i]:
+		q.append(li[i])
+	elif q[0] != li[i]:
+		q.appendleft(li[i])
+print(*q)
+
+
+
+

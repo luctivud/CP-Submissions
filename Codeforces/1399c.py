2805a21d038d2dc2d534917a640d8b75dba3b708
Udit Gupta <luctivud@gmail.com>
2021-04-14 10:58:31 +0530

    cf prc

---
+++ Codeforces/1399c.py
@@ -0,0 +1,44 @@
+#				 Author: Udit "luctivud" Gupta @ https://www.linkedin.com/in/udit-gupta-1b7863135/					 #
+
+
+import math;   		from collections import *
+import sys;   		from functools import reduce
+import time;   		from itertools import groupby
+
+# sys.setrecursionlimit(10**6)
+
+# def input()         : return sys.stdin.readline()
+def get_ints()      : return map(int, input().strip().split())
+def get_list()      : return list(get_ints())
+def get_string()    : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+DIRECTIONS = [(+0, +1), (+0, -1), (+1, +0), (+1, -1)] 
+NEIGHBOURS = [(-1, -1), (-1, +0), (-1, +1), (+0, -1),\
+              (+1, +1), (+1, +0), (+1, -1), (+0, +1)]
+
+
+
+# MAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+for _test_ in range(int(input())): 
+	n = int(input())
+	cnt = Counter(get_list())
+	ans = 0
+	for i in range(2, 2*n+1):
+		res = 0
+		for j in range(1, (i+1)//2):
+			res += min(cnt[j], cnt[i-j])
+		if not(i & 1):
+			res += cnt[i//2] // 2
+		# print(i, res)
+		ans = max(ans, res)
+
+	print(ans)
+
+
+
+
+

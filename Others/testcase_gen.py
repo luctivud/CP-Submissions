501daae7ddb04d8ba7ad865f0fae88043c5bdfb9
Udit Gupta <luctivud@gmail.com>
2021-04-25 15:23:05 +0530

    tcfl helper and generator

---
+++ Others/testcase_gen.py
@@ -0,0 +1,37 @@
+#				 Author: Udit "luctivud" Gupta @ https://www.linkedin.com/in/udit-gupta-1b7863135/					 #
+
+
+import math;   		from collections import *
+import sys;   		from functools import reduce
+import time;   		from itertools import groupby
+
+# sys.setrecursionlimit(10**6)
+
+# def input()         : return sys.stdin.readline()
+def get_ints()      : return map(int, input().strip().split())
+def get_list()      : return list(get_ints())
+def get_string()    : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+DIRECTIONS = [(+0, +1), (+0, -1), (+1, +0), (+1, -1)] 
+NEIGHBOURS = [(-1, -1), (-1, +0), (-1, +1), (+0, -1),\
+              (+1, +1), (+1, +0), (+1, -1), (+0, +1)]
+
+
+
+# MAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# for _test_ in range(int(input())): 
+import random
+
+T = random.randint(1000, 10000)
+print(T)
+for i in range(T):
+	print(random.randint(1, 179), random.randint(3, 10))
+
+
+
+
+

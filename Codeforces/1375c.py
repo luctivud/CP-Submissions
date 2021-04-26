9332500d73bc5ea26dc438a0188fc57917637705
Udit Gupta <luctivud@gmail.com>
2021-04-26 14:55:10 +0530

    prc

---
+++ Codeforces/1375c.py
@@ -0,0 +1,35 @@
+#				 Author: Udit "luctivud" Gupta @ https://www.linkedin.com/in/udit-gupta-1b7863135/					 #
+
+
+import math;   		from collections import *
+import sys;   		from functools import reduce
+import time;   		from itertools import groupby
+
+# sys.setrecursionlimit(10**6)
+
+# def input()         : return sys.stdin.readline()
+def get_ints()      : return map(int, input().strip().split())
+def get_list()      : return list(get_ints())
+def get_string()    : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+DIRECTIONS = [(+0, +1), (+0, -1), (+1, +0), (-1, -0)] 
+NEIGHBOURS = [(-1, -1), (-1, +0), (-1, +1), (+0, -1),\
+              (+1, +1), (+1, +0), (+1, -1), (+0, +1)]
+
+
+
+# MAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+for _test_ in range(int(input())): 
+	n = input()
+	li = get_list()
+	print("YES" if li[0] < li[-1] else "NO")
+
+
+
+
+
+

b73ff4068554e08b04b9930e2f73a1c37daebf3f
Udit Gupta <luctivud@gmail.com>
2021-05-09 08:29:29 +0530

    gfg contribute code

---
+++ Others/geeksandpairs.py
@@ -0,0 +1,62 @@
+''' 
+	************************.- J A I  S H R E E  R A M -.*************************
+
+	  ::: ---------- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	      Author   :  Udit "luctivud" Gupta 
+
+	      linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+	      github   :  https://github.com/luctivud
+
+	  :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ---------- :::
+
+'''
+
+import math, sys, collections, functools, time, itertools;
+
+# sys.setrecursionlimit(10**6)
+
+
+def Read_Ints()     : return map(int, input().strip().split())
+def Read_Array()    : return list(Read_Ints())
+def Read_Strings()  : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+
+d4i = [-1, +0, +1, +0]; d8i = [-1, -1, +0, +1, +1, +1, +0, -1]; 
+d4j = [+0, +1, +0, -1]; d8j = [+0, +1, +1, +1, +0, -1, -1, -1];
+
+
+# >>>>>>--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+def solveEachTest(_TestCase):
+	# printsp("Case #{}: ".format(_TestCase)) \
+	dp = [0 for x in range(21)]
+	for k in range(10):
+		dp[2*k] = dp[k]
+		dp[2*k+1] = dp[k] + 1
+	print(dp)
+
+	
+
+
+
+
+
+
+_T0T4 = 1;
+# _T0T4 = int(input()) 
+for _TestCase in range(1, _T0T4 + 1): 
+	solveEachTest(_TestCase)
+
+
+
+# 0.2s Domain Expansion  :
+#				  	MAHAYANA PRISON 
+

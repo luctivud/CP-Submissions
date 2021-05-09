b400ecf28a5c4c9c9da8ebf92b6815d75edf1aa0
Udit Gupta <luctivud@gmail.com>
2021-05-09 08:27:44 +0530

    topcoder template

---
+++ TopCoder/topcoder-template.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lld;
+
+class ${1:} {
+  public:
+
+};
+
+
+
+
+void readNumber(lld &N) {
+  cin >> N;
+}
+
+void readVector(vector<lld> arr) {
+  for (auto &i : arr) {
+    cin >> i;
+  }
+}
+
+
+
+signed main() {
+  ${1:} obj;
+
+  lld N; vector<lld> arr(N);
+  readNumber(N); readVector(arr);
+
+
+  return 0;
+}

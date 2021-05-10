2d4951552942822f4da90560d4d9e744103cc044
Udit Gupta <luctivud@gmail.com>
2021-05-10 15:27:16 +0530

    arc118

---
+++ AtCoder/arc118_a.py
@@ -0,0 +1,74 @@
+''' 
+	************************.- J A I  S H R E E  R A M -.*************************
+
+	  ::: ---------- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	      Author   :  Udit "luctivud" Gupta 
+
+	      linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+	      github   :  https://github.com/luctivud
+
+	  :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ---------- :::
+
+'''
+
+import math, sys, collections, functools, time, itertools;
+
+# sys.setrecursionlimit(10**6)
+
+
+def Read_Ints()     : return map(int, input().strip().split())
+def Read_Array()    : return list(Read_Ints())
+def Read_Strings()  : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+
+d4i = [-1, +0, +1, +0]; d8i = [-1, -1, +0, +1, +1, +1, +0, -1]; 
+d4j = [+0, +1, +0, -1]; d8j = [+0, +1, +1, +1, +0, -1, -1, -1];
+
+
+# >>>>>>--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+def solveEachTest(_TestCase):
+	# printsp("Case #{}: ".format(_TestCase)) 
+	t, n = Read_Ints()
+	t += 100
+	se = set()
+	for i in range(1, 1000000):
+		se.add(math.floor((t * i) / 100))
+		
+	se2 = []
+	for i in range(1, 1000000):
+		if len(se2) == 100:
+			break
+		if i not in se:
+			se2.append(i)
+	res = 0
+	for i in range(len(se2)-1):
+		res += se2[i+1] - se2[i]
+		if i == 14:
+			print(se2)
+
+	fac = res / (len(se2) - 1)
+	print(int(fac * n))
+
+
+
+
+
+_T0T4 = 1;
+# _T0T4 = int(input()) 
+for _TestCase in range(1, _T0T4 + 1): 
+	solveEachTest(_TestCase)
+
+
+
+# 0.2s Domain Expansion  :
+#				  	MAHAYANA PRISON 
+

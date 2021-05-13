ef433a13a1af7e373cd9c70540f3d30d6b4df028
Udit Gupta <luctivud@gmail.com>
2021-05-13 11:17:18 +0530

    Faster template

--- Others/FastTemplate.cpp
+++ Others/FastTemplate.cpp
@@ -35,14 +35,11 @@ typedef unsigned long long int llu;
 #define   for4(I7,S4,E4,S7)    for(auto I7=(S4); CH3K(I7,E4,S7); (I7)+=(S7))
 #define        forn(I7, E4)    for(lld I7=0ll; I7 < E4; (I7)+=1ll)
 #define       forn1(I7, E4)    for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
-#define        EACH(I7, A7)    for (auto& I7: A7)
 #define              len(v)    ((int)((v).size()))
 #define              all(x)    (x).begin(), (x).end()
 #define             rall(x)    (x).rbegin(), (x).rend()
-#define                  pb    push_back
 #define                  f1    first
 #define                  s2    second
-#define           PLEASE_AC    return
 
 
 
@@ -57,35 +54,6 @@ void huehue(istream_iterator<string> it, T a, Args... args) {
 
 const lld d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
 const lld d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
-
-
-const auto start_time = std::chrono::high_resolution_clock::now();
-
-signed _LIGHT_IO() {
-
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    cout.precision(10); cout << fixed;
-
-    #ifdef LUCTIVUD
-      freopen("/home/luctivud/CPPractice/Zinput.txt", "r", stdin);
-      freopen("/home/luctivud/CPPractice/Zoutput.txt", "w", stdout);
-    #endif
-
-    return 0; 
-}
-
-
-signed _LUCTIV_CRONO() {
-
-    #ifdef LUCTIVUD
-      auto end_time = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> diff = end_time - start_time;
-      cerr << "Finished in : " << diff.count() << "\n";
-    #endif
-
-  return 0; 
-}
-
 
 
 const lld MOD = lld(1e9) + 7ll;
@@ -106,8 +74,8 @@ const long double EPS = 1e-6;
 
 void solveEachTest(int _TestCase) {
     // cout << "Case #" << _TestCase << ": ";
-    
-		
+    	
+
 
     // cout << "\n"; 
     return;
@@ -115,14 +83,30 @@ void solveEachTest(int _TestCase) {
 
 
 signed main() {
-    _LIGHT_IO(); int _T0T4 = 1;
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    #ifdef LUCTIVUD
+      // const auto start_time = std::chrono::high_resolution_clock::now();
+      freopen("/home/luctivud/CPPractice/Zinput.txt", "r", stdin);
+      freopen("/home/luctivud/CPPractice/Zoutput.txt", "w", stdout);
+    #endif
+
+
+    lld _T0T4 = 1;
     cin >> _T0T4; 
 
     for (int _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
         solveEachTest(_TestCase);
     }
 
-    PLEASE_AC _LUCTIV_CRONO(); 
+
+    #ifdef LUCTIVUD
+      // auto end_time = std::chrono::high_resolution_clock::now();
+      // std::chrono::duration<double> diff = end_time - start_time;
+      // cerr << "Finished in : " << diff.count() << "\n";
+    #endif
+
+    return 0; 
 }
 
 /*        0.2s   Ryoki Tenkai:  

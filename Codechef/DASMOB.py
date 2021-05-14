6a3fc5e3d948cf57ae5a6197d29207d8ff13ab5f
Udit Gupta <luctivud@gmail.com>
2021-05-14 14:00:00 +0530

    BEST vs BEST contest

---
+++ Codechef/DASMOB.py
@@ -0,0 +1,68 @@
+''' 
+	************************.- J A I  S H R E E  R A M -.*************************
+
+	  ::: ---------- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	      Author   :  Udit "luctivud" Gupta 
+
+	      linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+	      github   :  https://github.com/luctivud
+
+	  :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ---------- :::
+
+'''
+
+import math, sys, collections, functools, time, itertools;
+
+# sys.setrecursionlimit(10**6)
+
+
+def Read_Ints()     : return map(int, input().strip().split())
+def Read_Array()    : return list(Read_Ints())
+def Read_Strings()  : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+
+d4i = [-1, +0, +1, +0]; d8i = [-1, -1, +0, +1, +1, +1, +0, -1]; 
+d4j = [+0, +1, +0, -1]; d8j = [+0, +1, +1, +1, +0, -1, -1, -1];
+
+
+# >>>>>>--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+def solveEachTest(_TestCase):
+	# printsp("Case #{}: ".format(_TestCase)) 
+	s = input()
+	num = cap = spasla = False;
+	# print(s)
+	for i in s:
+		if (i.isnumeric()):
+			num = True
+		elif (i.isalpha() and i.upper() == i):
+			cap = True
+		elif (i == ' ' or i == '/'):
+			spasla = True
+	# print(num, cap, spasla)
+	print("1" if (len(s) > 3 and num and cap and (not spasla) and (not s[0].isnumeric())) else "0");
+	
+
+
+
+
+
+
+_T0T4 = 1;
+_T0T4 = int(input()) 
+for _TestCase in range(1, _T0T4 + 1): 
+	solveEachTest(_TestCase)
+
+
+
+# 0.2s Domain Expansion  :
+#				  	MAHAYANA PRISON 
+

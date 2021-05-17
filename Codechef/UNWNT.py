76433f0155ae40987eeed68f42c1bc66ae278d39
Udit Gupta <luctivud@gmail.com>
2021-05-17 14:00:29 +0530

    codeshake2.0 rank-9

---
+++ Codechef/UNWNT.py
@@ -0,0 +1,44 @@
+#				 Author: Udit "luctivud" Gupta @ https://www.linkedin.com/in/udit-gupta-1b7863135/					 #
+
+
+import math;   		from collections import *
+import sys;   		from functools import reduce
+import time;   		from itertools import groupby
+
+# sys.setrecursionlimit(10**6)
+
+# def input()         : return sys.stdin.readline()
+def get_ints()      : return map(int, input().strip().split())
+def get_list()      : return list(get_ints())
+def get_string()    : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+DIRECTIONS = [(+0, +1), (+0, -1), (+1, +0), (-1, -0)] 
+NEIGHBOURS = [(-1, -1), (-1, +0), (-1, +1), (+0, -1),\
+              (+1, +1), (+1, +0), (+1, -1), (+0, +1)]
+
+
+
+# MAIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+for _test_ in range(int(input())): 	
+	s = input()
+	n = len(s)
+	Ncopy = int(s)
+	mn = s[0]
+	if int((mn * n)) > int(s):
+		mn = chr(ord(s[0])-1)
+	s = mn * n
+	if int(s) == 0:
+		s = '9' * (n-1)
+	# print("s", s)
+	n = len(s)
+	ans = 9 * (n-1)
+	ans += int(s[0])
+	print(Ncopy - ans)
+
+
+
+

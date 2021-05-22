53af97bd7eaa116bff81991617d589b381206c5e
Udit Gupta <luctivud@gmail.com>
2021-05-22 09:29:03 +0530

    fii code

---
+++ CSAcademy/alex-counts.py
@@ -0,0 +1,59 @@
+#   J A I  S H R E E  R A M   #
+
+
+import math, sys, collections, functools, time, itertools;
+
+# sys.setrecursionlimit(10**6)
+
+
+def Read_Ints()     : return map(int, input().strip().split())
+def Read_Array()    : return list(Read_Ints())
+def Read_Strings()  : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+
+d4i = [-1, +0, +1, +0]; d8i = [-1, -1, +0, +1, +1, +1, +0, -1]; 
+d4j = [+0, +1, +0, -1]; d8j = [+0, +1, +1, +1, +0, -1, -1, -1];
+
+
+# >>>>>>--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+def solveEachTest(_TestCase):
+	# printsp("Case #{}: ".format(_TestCase)) 
+	n = int(input())
+	arr = Read_Array()
+	ans = 0
+	li = []
+	cnt = collections.Counter(arr)
+	MX = max(arr)
+	import bisect
+	for i in range(n):
+		bisect.insort(li, arr[i])
+		# print(li)
+		for j in range(len(li)):
+			if arr[i] * li[j] <= MX:
+				if arr[i] * li[j] in cnt and arr[i] != li[j]:
+					ans += cnt[arr[i]*li[j]]
+			else:
+				break
+	print(ans)
+
+
+
+
+
+_T0T4 = 1;
+# _T0T4 = int(input()) 
+for _TestCase in range(1, _T0T4 + 1): 
+	solveEachTest(_TestCase)
+
+
+# Udit "luctivud" Gupta 
+# linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+

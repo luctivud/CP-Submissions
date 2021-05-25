40a07f918f9b8a2b52fda0446dda0261718a97b0
Udit Gupta <luctivud@gmail.com>
2021-05-25 16:29:14 +0530

    code it nerds contest

---
+++ Codechef/genPAirs_1529d.py
@@ -0,0 +1,55 @@
+#   J A I  S H R E E  R A M   #
+
+
+import math, sys, collections, functools, time, itertools;
+
+# sys.setrecursionlimit(10**6)
+
+
+def Read_Ints()     : return map(int, input().strip().split())
+def Read_Array()    : return list(Read_Ints())
+def Read_Strings()  : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+
+d4i = [-1, +0, +1, +0]; d8i = [-1, -1, +0, +1, +1, +1, +0, -1]; 
+d4j = [+0, +1, +0, -1]; d8j = [+0, +1, +1, +1, +0, -1, -1, -1];
+
+
+# >>>>>>--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+def solve(se, pairs):
+	if len(se) == 0:
+		
+
+
+
+def solveEachTest(_TestCase):
+	# printsp("Case #{}: ".format(_TestCase)) 
+	pairs = []
+	n = int(input())
+	ans = 0
+	se = set([i for i in range(2*n)])
+
+	solve(se, [])
+
+	print(ans)
+	
+
+
+
+
+
+
+_T0T4 = 1;
+_T0T4 = int(input()) 
+ans = 0
+for _TestCase in range(1, _T0T4 + 1): 
+	solveEachTest(_TestCase)
+
+
+# Udit "luctivud" Gupta 
+# linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+

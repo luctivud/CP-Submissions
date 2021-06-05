149b3bf99bfd97bcf61a33fb3841cac44e424540
Udit Gupta <luctivud@gmail.com>
2021-06-05 13:40:29 +0530

    cf edu round 1535

---
+++ Codeforces/1535c.cpp
@@ -0,0 +1,137 @@
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*:::::::::::::J A I  S H R E E  R A M::::::::::::::::*/
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LUCTIVUD
+    #include <buggyBaby.hpp>
+    pretty:: PrettyPrinter NonIterable;
+    #define cerr cout
+#else
+    #define _____error_____(...)
+#endif
+#pragma GCC optimize "trapv"
+
+
+/*:::::::::::::::::::SNIPS::::::::::::::::::::::::::::*/
+
+typedef long long lld; typedef unsigned long long llu;
+
+#define  forn(I7, E4) for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define forn1(I7, E4) for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
+#define        len(v) ((int)((v).size()))
+#define        all(x) (x).begin(), (x).end()
+#define       rall(x) (x).rbegin(), (x).rend()
+#define            f1 first
+#define            s2 second
+
+
+/*::::::::::::::::::/SNIPS::::::::::::::::::::::::::::*/
+/*:::::::::::::::::::UTILS::::::::::::::::::::::::::::*/
+
+
+const long double EPS = 1e-6;
+lld MOD = int(1e9) + 7;
+lld &mod = MOD;
+
+
+
+
+
+
+
+
+
+/*:::::::::::::::::::::/UTILS:::::::::::::::::::::::::*/
+/*::::::::::::::::::::::LOGIC:::::::::::::::::::::::::*/
+
+class Solution {
+    static const int maxN = int(1e5) + 2;
+public:
+    Solution() {
+    }
+public:
+    void SolveEachTest(int _TestCase) {
+        // cout << "Case #" << _TestCase << ":";    
+        string s; cin >> s;
+        lld n = len(s);
+        lld ans = 0ll;
+        lld ind = 0, prevQs = 0ll, sz = 0ll, st = 1;
+        while (ind < n) {
+            ans -= getCnt(prevQs);
+            while (ind < n) {
+                if (st == 0 and s[ind] != '?' and s[ind] == s[ind-1]) {
+                    break;
+                } else {
+                    st = 0;
+                    if (s[ind] == '?') {
+                        prevQs++;
+                        if (ind > 0 and s[ind-1] == '1') {
+                            s[ind] = '0';
+                        } else if (ind > 0 and s[ind-1] == '0') {
+                            s[ind] = '1';
+                        }
+                    } else {
+                        prevQs = 0ll;
+                    }
+                    ind++, sz++;
+                }
+            }
+            // _____error_____(sz, s, ind);
+            ans += getCnt(sz);
+            sz = prevQs;
+            st = 1;
+        }   
+        cout << ans;
+    }
+
+public:
+    void ResetTestCase() {
+        cout << "\n";
+    }
+
+    lld getCnt(lld sz) {
+        return (sz * (sz+1)) / 2;
+    }
+};
+
+
+
+/*:::::::::::::::::::::/LOGIC:::::::::::::::::::::::::*/
+/*::::::::::::::::::::::INVOC:::::::::::::::::::::::::*/
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    // cout.precision(10); cout << fixed;
+    #ifdef LUCTIVUD
+      // const auto start_time = std::chrono::high_resolution_clock::now();
+      freopen("/home/luctivud/CPPractice/Zinput.txt", "r", stdin);
+      freopen("/home/luctivud/CPPractice/Zoutput.txt", "w", stdout);
+    #endif
+
+
+    int _T0T4 = 1;
+    cin >> _T0T4; 
+    
+    Solution Aurora;
+    for (int _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
+        Aurora.SolveEachTest(_TestCase);
+        Aurora.ResetTestCase();
+    }
+
+
+    #ifdef LUCTIVUD
+      // auto end_time = std::chrono::high_resolution_clock::now();
+      // std::chrono::duration<double> diff = end_time - start_time;
+      // cerr << "Finished in : " << diff.count() << "\n";
+    #endif
+
+    return 0; 
+}
+
+/*:::::::::::::::::::::/INVOC:::::::::::::::::::::::::*/
+/*  ~~
+Author   :  Udit "luctivud" Gupta 
+linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+*/
+

3c8a405ccd887f8c447d03e18ed0e30d469c16f5
Udit Gupta <luctivud@gmail.com>
2021-06-13 20:30:07 +0530

    july long challenge samapti

--- Codechef/DUALDIST_1.cpp
+++ Codechef/DUALDIST_1.cpp
@@ -11,16 +11,16 @@ using namespace std;
 #else
     #define _____error_____(...)
 #endif
-// #pragma GCC optimize "trapv"
+#pragma GCC optimize "trapv"
 
 
 /*:::::::::::::::::::SNIPS::::::::::::::::::::::::::::*/
 
 typedef long long lld; typedef unsigned long long llu;
 
-#define  forn(I7, E4) for(lld I7=0; I7 < E4; (I7)+=1)
-#define forn1(I7, E4) for(lld I7=1; I7 < E4+1; (I7)+=1)
-#define        len(v) ((lld)((v).size()))
+#define  forn(I7, E4) for(int I7=0ll; I7 < E4; (I7)+=1)
+#define forn1(I7, E4) for(int I7=1ll; I7 < E4+1; (I7)+=1)
+#define        len(v) ((int)((v).size()))
 #define        all(x) (x).begin(), (x).end()
 #define       rall(x) (x).rbegin(), (x).rend()
 #define            f1 first
@@ -30,11 +30,13 @@ typedef long long lld; typedef unsigned long long llu;
 /*::::::::::::::::::/SNIPS::::::::::::::::::::::::::::*/
 /*:::::::::::::::::::UTILS::::::::::::::::::::::::::::*/
 
-
+// #define int int
 const long double EPS = 1e-6;
-lld MOD = lld(1e9) + 7;
-lld &mod = MOD;
-#define sz 1009
+int MOD = int(1e9) + 7;
+int &mod = MOD;
+const int MAXN = 1e5+2;
+#define sz 1001
+// vector<int> adj[MAXN];
 
 
 
@@ -43,80 +45,59 @@ lld &mod = MOD;
 /*::::::::::::::::::::::LOGIC:::::::::::::::::::::::::*/
 
 class Solution {
-    static const lld maxN = lld(1e5) + 2;
 public:
     Solution() {
     }
 public:
-    void SolveEachTest(lld _TestCase) {
+    void SolveEachTest(int _TestCase) {
         // cout << "Case #" << _TestCase << ":";
-        // memset(parent,-1,sizeof(parent));
-        lld n, q; cin >> n >> q;
-        // vector<lld> adj[n+1];
-        // vector<lld> depth(n+1, 0);
-        // vector<vector<lld>> parent(n+1, vector<lld>(level+1, -1));
-
-        // _____error_____(n, q);
-
-        forn(xx, n-1) {
-            lld a, b; cin >> a >> b;
-            // _____error_____(a, b);
+        int n, q;
+        cin >> n >> q;
+        forn(i, n-1) {
+            int a, b; cin >> a >> b;
             adj[a].push_back(b);
             adj[b].push_back(a);
+            // _____error_____(a, b);
         }
-
-        // dfs(1, 0);
-        // precomputeSparseMatrix(n, parent);
-        // precomputeSparseMatrix(n);
-
+        
         // performing required precalculations
         preprocess();
-
+     
         // doing the Euler walk
         ptr = 0;
         memset(FAI,-1,sizeof(FAI));
+        vector<int> depth(n+1, 0);
         dfs(1,0,0);
-
+        dfs1(1, 0, depth);
+     
         // creating depthArray corresponding to euler[]
         makeArr();
-
+     
         // building sparse table
-        buildSparseTable((lld)depthArr.size());
-
-        vector<vector<lld>> lca_of_ij(n+1, vector<lld> (n+1, 0));
-        // _____error_____(depth);
-        for (lld i = 1; i <= n; i++) {
-            for (lld j = 1; j <= n; j++) {
-                lca_of_ij[i][j] = LCA(i, j);
-                assert(lca_of_ij[i][j] > 0);
-                // cout << i << " " << j << " " << lca(i, j) << "\n";
-                // lca(i, j);
-            }
-        }
-
-        // NonIterable.print(lca_of_ij);
+        buildSparseTable((int)depthArr.size());
+        _____error_____(depthArr.size());
 
 
 
         forn(qq, q) {
-            lld a, b; cin >> a >> b;
-            lld ans = 0ll;
-            for (lld i = 1; i <= n; i++) {
-                lld laa, lbb;
-                laa = depth[i] + depth[a] - 2* depth[lca_of_ij[i][a]];
-                lbb = depth[i] + depth[b] - 2* depth[lca_of_ij[i][b]];
-                ans += min(laa, lbb);
-                // _____error_____(i, a, i, b);
-                _____error_____(laa, lbb);
-                // _____error_____(lca_of_ij[i][a], lca_of_ij[i][b]);
-                // _____error_____(depth[lca_of_ij[i][a]], depth[lca_of_ij[i][b]]);
+            int a, b; cin >> a >> b;
+            int res = 0ll;
+            // _____error_____(a, b);
+            int lucy = LCA(a, b);
+            _____error_____(a, b, lucy);
+            forn1(i, n) {
+                int dista = 0ll, distb = 0ll;
+                int lucya = LCA(i, a);
+                int lucyb = LCA(i, b);
+                dista = depthArr[i] + depthArr[a] - 2*depthArr[lucya];
+                distb = depthArr[i] + depthArr[b] - 2*depthArr[lucyb];
+                res += min(distb, dista);
+                _____error_____(i, a, lucya);
+                _____error_____(i, b, lucyb);
+                _____error_____(depthArr[i], depthArr[a], depthArr[b]);
+                _____error_____(dista, distb);
             }
-
-            cout << ans << "\n";
-        }
-
-        forn1(i, n) {
-            adj[i].clear();
+            cout << res << "\n";
         }
     }
 
@@ -125,61 +106,70 @@ public:
         // cout << "\n";
     }
 
-    lld depth[sz];
-    vector < lld > adj[sz]; // stores the tree
-    vector < lld > euler;    // tracks the eulerwalk
-    vector < lld > depthArr; // depth for each node corresponding
-                            // to eulerwalk
+    void dfs1(int node, int par, vector<int> &depth) {
+        depth[node] = depth[par] + 1;
+        for (auto i : adj[node]) {
+            if (node != par) {
+                dfs1(i, node, depth);
+            }
+        }
+    }
 
-    lld FAI[sz];     // stores first appearence index of every node
-    lld level[sz]; // stores depth for all nodes in the tree
-    lld ptr;         // pointer to euler walk
-    lld dp[sz][18]; // sparse table
-    lld logn[sz]; // stores log values
-    lld p2[20];  // stores power of 2
-
-    void buildSparseTable(lld n)
+    
+ 
+    vector < int > adj[sz];    // stores the tree
+    vector < int > euler;      // tracks the eulerwalk
+    vector < int > depthArr;   // depth for each node corresponding
+                               // to eulerwalk
+     
+    int FAI[sz];     // stores first appearence index of every node
+    int level[sz];   // stores depth for all nodes in the tree
+    int ptr;         // pointer to euler walk
+    int dp[sz][18];  // sparse table
+    int logn[sz];    // stores log values
+    int p2[20];      // stores power of 2
+     
+    void buildSparseTable(int n)
     {
         // initializing sparse table
         memset(dp,-1,sizeof(dp));
-
+     
         // filling base case values
-        for (lld i=1; i<n; i++)
+        for (int i=1; i<n; i++)
             dp[i-1][0] = (depthArr[i]>depthArr[i-1])?i-1:i;
-
+     
         // dp to fill sparse table
-        for (lld l=1; l<15; l++)
-        for (lld i=0; i<n; i++)
+        for (int l=1; l<15; l++)
+          for (int i=0; i<n; i++)
             if (dp[i][l-1]!=-1 and dp[i+p2[l-1]][l-1]!=-1)
-            dp[i][l] =
+              dp[i][l] =
                 (depthArr[dp[i][l-1]]>depthArr[dp[i+p2[l-1]][l-1]])?
-                dp[i+p2[l-1]][l-1] : dp[i][l-1];
+                 dp[i+p2[l-1]][l-1] : dp[i][l-1];
             else
-                break;
+                 break;
     }
-
-    lld query(lld l,lld r)
+     
+    int query(int l,int r)
     {
-        lld d = r-l;
-        lld dx = logn[d];
+        int d = r-l;
+        int dx = logn[d];
         if (l==r) return l;
         if (depthArr[dp[l][dx]] > depthArr[dp[r-p2[dx]][dx]])
             return dp[r-p2[dx]][dx];
         else
             return dp[l][dx];
     }
-
+     
     void preprocess()
     {
         // memorizing powers of 2
         p2[0] = 1;
-        for (lld i=1; i<18; i++)
+        for (int i=1; i<18; i++)
             p2[i] = p2[i-1]*2;
-
+     
         // memorizing all log(n) values
-        lld val = 1;
-        ptr=0;
-        for (lld i=1; i<sz; i++)
+        int val = 1,ptr=0;
+        for (int i=1; i<sz; i++)
         {
             logn[i] = ptr-1;
             if (val==i)
@@ -190,43 +180,42 @@ public:
             }
         }
     }
-
+     
     /**
-    * Euler Walk ( preorder traversal)
-    * converting tree to linear depthArray
-    * Time Complexity : O(n)
-    * */
-    void dfs(lld cur,lld prev,lld dep)
+     * Euler Walk ( preorder traversal)
+     * converting tree to linear depthArray
+     * Time Complexity : O(n)
+     * */
+    void dfs(int cur,int prev,int dep)
     {
         // marking FAI for cur node
-        depth[cur] = depth[prev] + 1;
         if (FAI[cur]==-1)
             FAI[cur] = ptr;
-
+     
         level[cur] = dep;
-
+     
         // pushing root to euler walk
         euler.push_back(cur);
-
+     
         // incrementing euler walk pointer
         ptr++;
-
+     
         for (auto x:adj[cur])
         {
             if (x != prev)
             {
                 dfs(x,cur,dep+1);
-
+     
                 // pushing cur again in backtrack
                 // of euler walk
                 euler.push_back(cur);
-
+     
                 // increment euler walk pointer
                 ptr++;
             }
         }
     }
-
+     
     // Create Level depthArray corresponding
     // to the Euler walk Array
     void makeArr()
@@ -234,21 +223,25 @@ public:
         for (auto x : euler)
             depthArr.push_back(level[x]);
     }
-
-    lld LCA(lld u,lld v)
+     
+    int LCA(int u,int v)
     {
         // trival case
         if (u==v)
-        return u;
-
+           return u;
+     
         if (FAI[u] > FAI[v])
-        swap(u,v);
-
+           swap(u,v);
+     
         // doing RMQ in the required range
         return euler[query(FAI[u], FAI[v])];
     }
-
-    
+     
+    void addEdge(int u,int v)
+    {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 };
 
 
@@ -265,13 +258,13 @@ signed main() {
     #endif
 
 
-    lld _T0T4 = 1;
+    int _T0T4 = 1;
     cin >> _T0T4; 
     
-    for (lld _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
+    for (int _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
         Solution Aurora;
         Aurora.SolveEachTest(_TestCase);
-        Aurora.ResetTestCase();
+        // Aurora.ResetTestCase();
     }
 
 

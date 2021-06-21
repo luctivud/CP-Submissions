a4b9020c60f27a8df3e40128dd3f3b5f488c7336
Udit Gupta <luctivud@gmail.com>
2021-06-21 10:14:06 +0530

    cf 1539

---
+++ Codeforces/1539d.cpp
@@ -0,0 +1,144 @@
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*:::::::::::::J A I  S H R E E  R A M::::::::::::::::*/
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LUCTIVUD
+    #include <buggyBaby.hpp>
+    pretty:: PrettyPrinter NonIterable;
+    #define cerr cout
+#else
+    #define _____error_____(...)
+#endif
+#pragma GCC optimize "trapv"
+
+
+/*:::::::::::::::::::SNIPS::::::::::::::::::::::::::::*/
+
+typedef long long lld; typedef unsigned long long llu;
+
+#define  forn(I7, E4) for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define forn1(I7, E4) for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
+#define        len(v) ((int)((v).size()))
+#define        all(x) (x).begin(), (x).end()
+#define       rall(x) (x).rbegin(), (x).rend()
+#define            f1 first
+#define            s2 second
+
+
+/*::::::::::::::::::/SNIPS::::::::::::::::::::::::::::*/
+/*:::::::::::::::::::UTILS::::::::::::::::::::::::::::*/
+
+
+const long double EPS = 1e-6;
+lld MOD = int(1e9) + 7;
+lld &mod = MOD;
+
+
+
+
+
+
+
+
+
+/*:::::::::::::::::::::/UTILS:::::::::::::::::::::::::*/
+/*::::::::::::::::::::::LOGIC:::::::::::::::::::::::::*/
+
+class Solution {
+    static const int maxN = int(1e5) + 2;
+public:
+    Solution() {
+    }
+public:
+    void SolveEachTest(int _TestCase) {
+        // cout << "Case #" << _TestCase << ":";
+        lld n; cin >> n;
+        vector<pair<lld, lld>> arr(n);
+        lld ans = 0ll;
+        forn(i, n) {
+            cin >> arr[i].f1 >> arr[i].s2;
+            ans += (arr[i].f1 << 1ll);
+        }
+        sort(all(arr), [](auto &a, auto &b) {
+            return a.s2 == b.s2 ? a.f1 > b.f1 : a.s2 < b.s2;
+        });
+
+        // NonIterable.print(arr);
+
+        lld Lind = 0ll, Rind = n-1;
+        lld currTaken = 0ll;
+        lld RemTaken = (ans >> 1ll);
+        while (Lind <= Rind) {
+            if (currTaken < arr[Lind].s2) {
+                lld GopuCh = min(arr[Lind].s2 - currTaken, RemTaken);
+                currTaken += GopuCh;
+                RemTaken -= GopuCh;
+                while (GopuCh > 0) {
+                    lld ThisGopuCh = min(GopuCh, arr[Rind].f1);
+                    arr[Rind].f1 -= ThisGopuCh;
+                    GopuCh -= ThisGopuCh;
+                    if (arr[Rind].f1 == 0)
+                        Rind -= 1;
+                }
+                ans -= arr[Lind].f1;
+                currTaken += arr[Lind].f1;
+                arr[Lind].s2 = 0;
+                Lind++;
+            } else {
+                ans -= arr[Lind].f1;
+                currTaken += arr[Lind++].f1;
+            }
+            _____error_____(Lind, Rind, ans, currTaken);
+            _____error_____(arr);
+        }
+
+        cout << ans;
+    }
+
+public:
+    void ResetTestCase() {
+        cout << "\n";
+    }
+};
+
+
+
+/*:::::::::::::::::::::/LOGIC:::::::::::::::::::::::::*/
+/*::::::::::::::::::::::INVOC:::::::::::::::::::::::::*/
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    // cout.precision(10); cout << fixed;
+    #ifdef LUCTIVUD
+      // const auto start_time = std::chrono::high_resolution_clock::now();
+      freopen("/home/luctivud/CPPractice/Zinput.txt", "r", stdin);
+      freopen("/home/luctivud/CPPractice/Zoutput.txt", "w", stdout);
+    #endif
+
+
+    int _T0T4 = 1;
+    // cin >> _T0T4; 
+    
+    Solution Aurora;
+    for (int _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
+        Aurora.SolveEachTest(_TestCase);
+        Aurora.ResetTestCase();
+    }
+
+
+    #ifdef LUCTIVUD
+      // auto end_time = std::chrono::high_resolution_clock::now();
+      // std::chrono::duration<double> diff = end_time - start_time;
+      // cerr << "Finished in : " << diff.count() << "\n";
+    #endif
+
+    return 0; 
+}
+
+/*:::::::::::::::::::::/INVOC:::::::::::::::::::::::::*/
+/*  ~~
+Author   :  Udit "luctivud" Gupta 
+linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+*/
+

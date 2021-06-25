91f02f0e285fdc0e7447718fa64955c7342ae5d2
Udit Gupta <luctivud@gmail.com>
2021-06-25 16:14:42 +0530

    june challenge

--- HackerEarth/square-numbers.cpp
+++ HackerEarth/square-numbers.cpp
@@ -41,20 +41,115 @@ lld &mod = MOD;
 
 
 
+/* This part should be outside the main in global paradigm. */
+
+const long long MAXN = (lld)(1e4) + 1ll; // MAXN Size
+
+vector<long long >isPrime(MAXN , true); // checkIfPrime
+vector<long long >prime_numbers; // List of prime numbers
+vector<long long >smallest_prime_factor(MAXN); // smallest_prime_factor of a number
+
+
+void manipulated_seive() {
+    isPrime[0] = isPrime[1] = false ;
+    
+    prime_numbers.push_back(2);
+    smallest_prime_factor[2] = 2ll;
+
+    for (long long int i=4; i < MAXN ; i+=2) {
+        isPrime[i] = false;
+        smallest_prime_factor[i] = 2ll;
+    }
+
+    for (long long int i = 3; i < MAXN ; i+=2) {
+        if (isPrime[i]) {
+            prime_numbers.push_back(i);
+            smallest_prime_factor[i] = i;
+        }
+
+        for (long long int j = 0; j < (int)prime_numbers.size() && i * prime_numbers[j] < MAXN && prime_numbers[j] <= smallest_prime_factor[i]; j++) {
+            isPrime[i * prime_numbers[j]] = false;
+            smallest_prime_factor[i * prime_numbers[j]] = prime_numbers[j] ;
+        }
+    }
+}
+
+
+/* This should be called inside main. */
+    
+
+
+
+
 
 
 /*:::::::::::::::::::::/UTILS:::::::::::::::::::::::::*/
 /*::::::::::::::::::::::LOGIC:::::::::::::::::::::::::*/
 
 class Solution {
-    static const int maxN = int(1e5) + 2;
+    static const int maxN = (lld)(31623) + 1ll;
 public:
     Solution() {
+        manipulated_seive();
     }
 public:
     void SolveEachTest(int _TestCase) {
         // cout << "Case #" << _TestCase << ":";
-        
+        lld n; cin >> n;
+        vector<lld> arr(n);
+        map<lld, lld> cnt;
+        forn(i, n) {
+            cin >> arr[i];
+        }
+        // cout << sqrt(1e9);
+        // _____error_____(len(prime_numbers));
+        vector<lld> brr(n, 1);
+        for (auto num : prime_numbers) {
+            if (num > maxN) {
+                break;
+            }
+            forn(i, n) {
+                if (arr[i] % num == 0) {
+                    brr[i] *= num;
+                    while (arr[i] % num == 0) {
+                        arr[i] /= num;
+                    }
+                }
+            }
+        }
+
+        forn(i, n) {
+            if (arr[i] > 1) {
+                cnt[arr[i]] += 1;
+            }
+        }
+
+        forn(i, n) {
+            for (lld j = 1; j * j <= brr[i]; j += 1) {
+                if (brr[i] % j == 0) {
+                    cnt[j] += 1;
+                    if (brr[i] / j != j) {
+                        cnt[brr[i]/j] += 1;
+                    }
+                }
+                // if (j > 2) j++;
+            }
+        }
+        _____error_____(arr);
+        _____error_____(brr);
+        _____error_____(cnt);
+
+        lld mxNum = 0ll, mxNumCnt = 0ll;
+        for (auto [ke, va] : cnt) {
+            if (ke == 1) continue;
+            if (va > mxNum) {
+                mxNum = va;
+                mxNumCnt = 1;
+            } else if (va == mxNum) {
+                mxNumCnt += 1;
+            }
+        }
+        cout << mxNum << " " << mxNumCnt;
     }
 
 public:

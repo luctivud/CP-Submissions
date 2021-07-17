2febc10e95556e60a8d5e5dea95a02e64588da43
Udit Gupta <luctivud@gmail.com>
2021-07-17 11:46:55 +0530

    NI Interview

---
+++ Others/chessgame.cpp
@@ -0,0 +1,42 @@
+class ChessBoard {
+    int ChessBoard[8][8];
+
+    makemove(currentPosition, nextPosition) {
+        // get the information of type of piece at currentPosition
+        switch (case ) {
+
+        }
+        if checkValid() {
+
+        } else {
+
+        }
+    }
+}
+
+
+
+
+class Piece {
+    pair<int, int> currentPosition;
+    pair<int, int> nextPosition;
+    interface checkValid(nextPosition) 
+}
+
+class Queen inherit Piece implement checkValid {
+    checkValid(currentPosition, nextPosition) {
+
+    }
+}
+
+class King inherit Piece implement checkValid {
+    checkValid(currentPosition, nextPosition) {
+
+    }
+}
+
+
+//  
+
+// clock = 0
+// i

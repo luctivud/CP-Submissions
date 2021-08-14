b09335a131f282ce7b6a0eae877825830faf46b2
Udit Gupta <luctivud@gmail.com>
2021-08-14 14:12:48 +0530

    codechef may lunchtime

---
+++ Codechef/CHFINVNT.cpp
@@ -0,0 +1,201 @@
+// ------------------------------------------------------------------- //
+/*
+
+      Author :  Udit "luctivud" Gupta
+    Linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+      Github :  https://github.com/luctivud
+
+*/
+// ------------------------------------------------------------------- //
+
+
+
+/*          J  A  I   S  H  R  E  E   R  A  M          */
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LUCTIVUD
+#include <buggyBaby.hpp>
+pretty:: PrettyPrinter NonIterable;
+#define cerr cout
+#else
+#define _____error_____(...)
+#endif
+
+#pragma GCC optimize("O3,unroll-loops,trapv")
+#pragma GCC target("avx,avx2,fma,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
+// #pragma GCC optimize "trapv"
+
+
+
+
+
+
+/*  Snips   */
+
+
+typedef long long lld; typedef unsigned long long llu;
+
+#define  forn(I7, E4) for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define forn1(I7, E4) for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
+#define        len(v) ((int)((v).size()))
+#define        all(x) (x).begin(), (x).end()
+#define       rall(x) (x).rbegin(), (x).rend()
+#define            f1 first
+#define            s2 second
+
+
+
+/*  Utils   */
+
+
+const lld MOD = int(1e9) + 7;
+const lld &mod = MOD;
+
+const long double EPS = 1e-6;
+
+const lld inf = 2e18;
+
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+
+signed pleaseAC(void) {
+
+#ifdef LUCTIVUD
+    // auto end_time = std::chrono::high_resolution_clock::now();
+    // std::chrono::duration<double> diff = end_time - start_time;
+    // cerr << "Finished in : " << diff.count() << "\n";
+#endif
+
+    return 0;
+}
+
+
+void IAmJustice(void) {
+
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cout.precision(10); cout << fixed;
+
+#ifdef LUCTIVUD
+    // const auto start_time = std::chrono::high_resolution_clock::now();
+    freopen("/home/luctivud/CPPractice/IO/Zinput.txt", "r", stdin);
+    freopen("/home/luctivud/CPPractice/IO/Zoutput.txt", "w", stdout);
+#endif
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*:::::::::::::::::::::: LOGIC :::::::::::::::::::::::::*/
+
+
+
+
+
+void solveEachTest(int _TestCase) {
+    // cout << "Case #" << _TestCase << ": ";
+    lld n, p, k; cin >> n >> p >> k;
+    lld ans = 0ll;
+    forn(i, p % k) {
+        // _____error_____((n - i + k - 1) / k, i);
+        ans += (n - i + k - 1) / k;
+    }
+
+
+    ans += (p / k) + 1;
+
+    cout << ans;
+
+
+    return;
+}
+
+
+
+/*:::::::::::::::::::::/LOGIC:::::::::::::::::::::::::*/
+
+
+signed main() {
+
+    IAmJustice();
+
+    int _T0T4 = 1;
+    cin >> _T0T4;
+
+    for (int _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
+        solveEachTest(_TestCase);
+        cout << "\n";
+    }
+
+    return pleaseAC();
+}
+
+// lld mn = (n % k) * ((p + k - 1) / k);
+// ans += mn;
+// p -= (n % k);
+// _____error_____(p);
+
+// mn = (p + k - 1) / k;
+// while (p > k) {
+//     lld mnnn = min(k, p);
+//     p -= mnnn;
+//     mn++;
+//     ans += mn * mnnn;
+// }
+
+
+
+/*  ~~
+    I ponder of something great
+    My lungs will fill and then deflate
+    They fill with fire, exhale desire
+    I know it's dire my time today
+
+    I have these thoughts, so often I ought
+    To replace that slot with what I once bought
+    'Cause somebody stole my car radio
+    And now I just sit in silence
+
+    Sometimes quiet is violent
+    I find it hard to hide it
+    My pride is no longer inside
+    It's on my sleeve
+    My skin will scream reminding me of
+    Who I killed inside my dream
+    I hate this car that I'm driving
+    There's no hiding for me
+    I'm forced to deal with what I feel
+    There is no distraction to mask what is real
+    I could pull the steering wheel
+
+*/

6254be7a107df1455bc06722cfb3872e7498fff0
Udit Gupta <luctivud@gmail.com>
2021-09-04 23:35:04 +0530

    sept easy

---
+++ HackerEarth/prime-sum-7.cpp
@@ -0,0 +1,179 @@
+/*          J  A  I   S  H  R  E  E   R  A  M          */
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LUCTIVUD
+#include <buggyBaby.hpp>
+pretty:: PrettyPrinter NonIterable;
+#define cerr cout
+#else
+#define _____error_____(...)
+#endif
+
+
+#pragma GCC optimize("O3,unroll-loops,trapv")
+#pragma GCC target("avx,avx2,fma,sse,sse2,sse3,sse4,popcnt,lzcnt")
+// #pragma GCC optimize "trapv"
+
+
+
+typedef long long lld; typedef unsigned long long llu;
+
+#define  forn(I7, E4) for(lld I7=0ll; I7 < E4; (I7)+=1ll)
+#define forn1(I7, E4) for(lld I7=1ll; I7 < E4+1; (I7)+=1ll)
+#define        len(v) ((int)((v).size()))
+#define        all(x) (x).begin(), (x).end()
+#define       rall(x) (x).rbegin(), (x).rend()
+#define            f1 first
+#define            s2 second
+
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+
+
+
+const lld MOD = int(1e9) + 7;
+const lld &mod = MOD;
+
+const long double EPS = 1e-6;
+
+const lld inf = 2e18;
+
+
+
+
+
+
+
+
+/* This part should be outside the main in global paradigm. */
+
+const long long MAXN = (lld)(1e7) + 1ll; // MAXN Size
+
+vector<long long >isPrime(MAXN , true); // checkIfPrime
+vector<long long >prime_numbers; // List of prime numbers
+vector<long long >smallest_prime_factor(MAXN); // smallest_prime_factor of a number
+
+
+void manipulated_seive() {
+    isPrime[0] = isPrime[1] = false ;
+
+    prime_numbers.push_back(2);
+    smallest_prime_factor[2] = 2ll;
+
+    for (long long int i = 4; i < MAXN ; i += 2) {
+        isPrime[i] = false;
+        smallest_prime_factor[i] = 2ll;
+    }
+
+    for (long long int i = 3; i < MAXN ; i += 2) {
+        if (isPrime[i]) {
+            prime_numbers.push_back(i);
+            smallest_prime_factor[i] = i;
+        }
+
+        for (long long int j = 0; j < (int)prime_numbers.size() && i * prime_numbers[j] < MAXN && prime_numbers[j] <= smallest_prime_factor[i]; j++) {
+            isPrime[i * prime_numbers[j]] = false;
+            smallest_prime_factor[i * prime_numbers[j]] = prime_numbers[j] ;
+        }
+    }
+}
+
+
+
+
+
+
+/*:::::::::::::::::::::: LOGIC :::::::::::::::::::::::::*/
+
+
+
+
+
+void solveEachTest(int _TestCase) {
+    // cout << "Case #" << _TestCase << ": ";
+
+    lld n; cin >> n;
+
+    lld sz = prime_numbers.size();
+    lld ans = 0ll;
+    forn(i, 1) {
+        if (prime_numbers[i] > n) {
+            break;
+        }
+        for (lld j = i+1; j < sz; j++) {
+            if (prime_numbers[i] + prime_numbers[j] > n) {
+                break;
+            }
+            _____error_____(prime_numbers[i], prime_numbers[j]);
+            ans += isPrime[prime_numbers[i] + prime_numbers[j]];
+        }
+    }
+
+    cout << ans;
+
+    return;
+}
+
+
+
+/*:::::::::::::::::::::/LOGIC:::::::::::::::::::::::::*/
+
+
+signed main() {
+
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cout.precision(10); cout << fixed;
+
+#ifdef LUCTIVUD
+    // const auto start_time = std::chrono::high_resolution_clock::now();
+    freopen("/home/luctivud/CPPractice/Zinput.txt", "r", stdin);
+    freopen("/home/luctivud/CPPractice/Zoutput.txt", "w", stdout);
+#endif
+
+
+    /* This should be called inside main. */
+    manipulated_seive();
+
+
+
+    int _T0T4 = 1;
+    // cin >> _T0T4;
+
+    for (int _TestCase = 1; _TestCase <= _T0T4; _TestCase++) {
+        solveEachTest(_TestCase);
+        cout << "\n";
+    }
+
+
+#ifdef LUCTIVUD
+    // auto end_time = std::chrono::high_resolution_clock::now();
+    // std::chrono::duration<double> diff = end_time - start_time;
+    // cerr << "Finished in : " << diff.count() << "\n";
+#endif
+
+
+    return 0;
+}
+
+
+
+
+/*  ~~
+    .?.?.?.
+*/

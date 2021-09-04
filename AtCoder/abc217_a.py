e73db4ede3e4ae43bca87fe5c480b65190f18f01
Udit Gupta <luctivud@gmail.com>
2021-09-04 23:35:35 +0530

    abc217

---
+++ AtCoder/abc217_a.py
@@ -0,0 +1,47 @@
+#   J A I  S H R E E  R A M   #
+
+
+import math, sys, collections, functools, time, itertools;
+
+# sys.setrecursionlimit(10**6)
+
+
+def Read_Ints()     : return map(int, input().strip().split())
+def Read_Array()    : return list(Read_Ints())
+def Read_Strings()  : return list(input().strip().split())
+def printxsp(*args) : return print(*args, end="")
+def printsp(*args)  : return print(*args, end=" ")
+
+
+
+d4i = [-1, +0, +1, +0]; d8i = [-1, -1, +0, +1, +1, +1, +0, -1]; 
+d4j = [+0, +1, +0, -1]; d8j = [+0, +1, +1, +1, +0, -1, -1, -1];
+
+
+# >>>>>>--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+def solveEachTest(_TestCase):
+    # printsp("Case #{}: ".format(_TestCase)) 
+    a, b = input().split()
+    print("Yes" if a < b else "No")
+    
+    dq = collections.deque()
+    sorted(dq)
+
+
+
+
+
+_T0T4 = 1;
+# _T0T4 = int(input()) 
+for _TestCase in range(1, _T0T4 + 1): 
+    solveEachTest(_TestCase)
+
+
+# Udit "luctivud" Gupta time
+# linkedin :  https://www.linkedin.com/in/udit-gupta-1b7863135/
+
